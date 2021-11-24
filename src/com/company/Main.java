34877cdcfc707082b6fac461130a466365cb7206
fritzkuechli <fritzchensrevenge@gmail.com>
2021-11-24 08:50:58 +0100

    pseudo-code image changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    int size = 100;
+	int [] list = new int[size];
+    Set<Integer> set = new HashSet<Integer>();
+    while (set.size() != size){
+
+    }
+
     }
 }
